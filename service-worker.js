d2253493bc7ac9aa4adcfa11f922a9a67a7394ab
yanaqueen <yana.queen@mail.com>
2025-06-28 04:48:38 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,31 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker Installed');
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('app-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png',
+        'J1.jpg',
+        'J2.jpg',
+        'J3.jpg',
+        'J4.jpg',
+        'J5.jpg',
+        'J7.jpg',
+        'Y1.jpg'
+      ]);
+
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
